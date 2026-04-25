51111c698f8486f2e3f1da0a06eaac1181581b23
jj1eckhardt-beep <jj1eckhardt@gmail.com>
2026-04-25 17:35:17 -0600

    Add files via upload

--- Launcher.bat
+++ Launcher.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0v1.1.0.ps1""' -Verb RunAs"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0ArkOS-Utility.ps1""' -Verb RunAs"
